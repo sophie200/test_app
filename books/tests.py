c2dac84a67821a9aee40e960ce6e39eab0148048
sophie200 <44643926+sophie200@users.noreply.github.com>
2021-06-24 23:43:12 -0500

    column deletion

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Author
+
+class ReporterTests(TestCase):
+
+    def setUp(self):
+        self.payment = Author.objects.create(first_name = 'Bob', last_name = 'Ross')
+    
+    def test_reporter(self):
+        self.assertEqual(f'{self.first_name}', 'Bob')
