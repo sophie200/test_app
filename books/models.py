959f98ea797e51391637443af61d39891b99673f
sophie200 <44643926+sophie200@users.noreply.github.com>
2021-06-25 23:40:26 -0500

    rename first_name to fname

--- books/models.py
+++ books/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=30)
+    fname = models.CharField(max_length=30)
 
 class Book(models.Model):
     title = models.CharField(max_length=30)
